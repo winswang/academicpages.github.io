89531939ea582cab9d256eb40e4ddf2b27d13277
winswang <winstwang@icloud.com>
2017-10-12 11:17:34 -0500

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,16 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "San Francisco CA, USA",
+    37.7579942,
+    -122.577685
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Jeju Island, South Korea",
+    33.4766982,
+    126.4728636
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
-];
+    "Anaheim CA, USA",
+    33.8341966,
+    -117.9855787
+  ];
